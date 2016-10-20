6809e27862a81885387fad8cc4e9ee5d2b82fa83
WuKongCoo1 <wu_kong_coo1@163.com>
2016-10-20 16:49:18 +0800

    Add test file

---
+++ WKVideoCapturer/Classes/WKVideoCapturer.h
@@ -0,0 +1,13 @@
+//
+//  WKVideoCapturer.h
+//  Pods
+//
+//  Created by 吴珂 on 2016/10/20.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WKVideoCapturer : NSObject
+
+@end
---
+++ WKVideoCapturer/Classes/WKVideoCapturer.m
@@ -0,0 +1,13 @@
+//
+//  WKVideoCapturer.m
+//  Pods
+//
+//  Created by 吴珂 on 2016/10/20.
+//
+//
+
+#import "WKVideoCapturer.h"
+
+@implementation WKVideoCapturer
+
+@end
